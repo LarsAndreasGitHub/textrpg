3dc241547d0e11f3818907015e50ebd0f629c64a
Lars Andreas Tveiten <lat@forskningsradet.no>
2018-04-27 17:34:19 +0200

    Gjør klar til utvikling

---
+++ src/home.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class Home extends React.Component {
+  public render() {
+    return (
+      <div>
+          hei :)
+      </div>
+    );
+  }
+}
+
+export default Home;
